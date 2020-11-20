9e974a8c625357a2343c21d31c04aab35a82fd2b
Remisse <zeromuseg7@gmail.com>
2020-11-20 23:11:55 +0100

    Complete exercise 3

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,31 +1,59 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
- * 
+ * Interface modelling a simple controller responsible of I/O access. It considers a
+ * single file at a time, and it is able to serialize objects in it.
  */
 public class Controller {
 
-    /*
-     * This class must implement a simple controller responsible of I/O access. It
-     * considers a single file at a time, and it is able to serialize objects in it.
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String DEFAULT_PATH = System.getProperty("user.home");
+    private static final String DEFAULT_FILE = DEFAULT_PATH + SEPARATOR + "output.txt";
+    private File currentFile = new File(DEFAULT_FILE);
+
+    /**
+     * Sets a new {@code currentFile}.
      * 
-     * Implement this class with:
-     * 
-     * 1) A method for setting a File as current file
-     * 
-     * 2) A method for getting the current File
-     * 
-     * 3) A method for getting the path (in form of String) of the current File
-     * 
-     * 4) A method that gets a String as input and saves its content on the current
-     * file. This method may throw an IOException.
-     * 
-     * 5) By default, the current file is "output.txt" inside the user home folder.
-     * A String representing the local user home folder can be accessed using
-     * System.getProperty("user.home"). The separator symbol (/ on *nix, \ on
-     * Windows) can be obtained as String through the method
-     * System.getProperty("file.separator"). The combined use of those methods leads
-     * to a software that runs correctly on every platform.
+     * @param file
+     *          {@link File} to be set as {@code currentFile}.
      */
+    public final void setCurrentFile(final File file) {
+        this.currentFile = file;
+    }
+
+    /**
+     * Returns the {@link File} set as {@code currentFile}.
+     * 
+     * @return the field {@code currentFile}.
+     */
+    public final File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    /**
+     * Returns the path on disk of the file set as {@code currentFile}.
+     * 
+     * @return a {@code String} containing the path to {@code currentFile}.
+     */
+    public final String getPathOfCurrentFile() {
+        return this.currentFile.getPath();
+    }
+
+    /**
+     * Writes the content of {@code text} to the {@code currentFile}.
+     * 
+     * @param text
+     *          {@code String} to be written.
+     */
+    public final void writeOnCurrentFile(final String text) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.currentFile))) {
+            writer.write(text);
+        }
+    }
 
 }
--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,9 +1,18 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -12,6 +21,7 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller;
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -32,11 +42,48 @@ public final class SimpleGUI {
      * 
      * Use "ex02.png" (in the res directory) to verify the expected aspect.
      */
+    private void arrangeComponents() {
+        final JPanel mainPanel = new JPanel(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        final JButton bSave = new JButton("Save");
+        mainPanel.add(textArea);
+        mainPanel.add(bSave, BorderLayout.SOUTH);
+        this.frame.setContentPane(mainPanel);
+
+        bSave.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    SimpleGUI.this.controller.writeOnCurrentFile(textArea.getText());
+                } catch (IOException exception) {
+                    JOptionPane.showMessageDialog(SimpleGUI.this.frame, exception, "Error", JDialog.EXIT_ON_CLOSE);
+                    exception.printStackTrace();
+                }
+            }
+        });
+        /*
+        bSave.addActionListener(e -> {
+            try {
+                this.controller.writeOnCurrentFile(textArea.getText());
+            } catch (IOException exception) {
+                JOptionPane.showMessageDialog(this.frame, exception, "Error", JDialog.EXIT_ON_CLOSE);
+                exception.printStackTrace();
+            }
+        });
+        */
+    }
 
     /**
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
+        /*
+         * Creating a new Controller.
+         */
+        this.controller = new Controller();
+        /*
+         * Laying out the various GUI components as per instructions.
+         */
+        this.arrangeComponents();
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -50,13 +97,24 @@ public final class SimpleGUI {
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
-        frame.setSize(sw / 2, sh / 2);
+        this.frame.setSize(sw / 2, sh / 2);
         /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
          * flag makes the OS window manager take care of the default positioning
          * on screen. Results may vary, but it is generally the best choice.
          */
-        frame.setLocationByPlatform(true);
+        this.frame.setLocationByPlatform(true);
+        /*
+         * Will exit without confirmation from the user.
+         */
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * "Activates" the application window.
+         */
+        this.frame.setVisible(true);
     }
 
+    public static void main(final String... args) {
+        new SimpleGUI();
+    }
 }

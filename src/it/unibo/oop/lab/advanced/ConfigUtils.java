6031bdaff740b0a86a87a76a3ed3f64a0924bd86
Remisse <zeromuseg7@gmail.com>
2020-11-22 16:16:07 +0100

    Trim doYAMLToMap() and add methods for generating default config file

--- src/it/unibo/oop/lab/advanced/ConfigUtils.java
+++ src/it/unibo/oop/lab/advanced/ConfigUtils.java
@@ -1,20 +1,19 @@
 package it.unibo.oop.lab.advanced;
 
-import java.awt.geom.IllegalPathStateException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static it.unibo.oop.lab.advanced.Settings.*;
-
 public final class ConfigUtils {
 
+    private static final String SPLITTER = ":";
     private static final String SEP = System.getProperty("file.separator");
     private static final Path CONFIG_PATH = Paths.get("res" + SEP + "config.yml");
 
@@ -24,25 +23,31 @@ public final class ConfigUtils {
     private ConfigUtils() {
     }
 
-    private static String getDefaultConfigString() {
+    private static List<String> getDefaultConfig() {
         return Stream.of(Settings.values())
-                     .map(stg -> stg.getName() + ": " + stg.getDefaultValue())
-                     .collect(Collectors.joining(","));
+                     .map(stg -> stg.getName() + SPLITTER + "\s" + stg.getDefaultValue())
+                     .collect(Collectors.toList());
+    }
+    
+    private static void generateDefaultConfigFile() throws IOException {
+        Files.write(CONFIG_PATH, getDefaultConfig(), StandardOpenOption.CREATE_NEW,
+                                                     StandardOpenOption.WRITE);
     }
 
     private static Map<Settings, Integer> doYAMLToMap() throws IOException {
-        return Pattern.compile(",")
-                      .splitAsStream(Files.lines(CONFIG_PATH)
-                                          .collect(Collectors.joining(",")))
-                      .map(s -> s.replace("\s", ""))
-                      .map(s -> s.split(":"))
-                      .collect(Collectors.toMap(s -> Settings.of(s[0]),
-                                                s -> Integer.valueOf(s[1]),
-                                                (x, y) -> y,
-                                                () -> new EnumMap<>(Settings.class)));
+        return Files.lines(CONFIG_PATH)
+                    .map(s -> s.replace("\s", "")) //Delete all whitespaces
+                    .map(s -> s.split(SPLITTER))
+                    .collect(Collectors.toMap(s -> Settings.of(s[0]),
+                                              s -> Integer.valueOf(s[1]),
+                                              (x, y) -> y, //If there's a duplicate setting, get the last occurrence
+                                              () -> new EnumMap<>(Settings.class)));
     }
-
-    public static Map<Settings, Integer> readSettings() throws IOException {
+    
+    public static Map<Settings, Integer> readConfig() throws IOException {
+        if (Files.notExists(CONFIG_PATH)) {
+            generateDefaultConfigFile();
+        }
         return doYAMLToMap();
     }
 }
